b55e98da23ca4e4f5b8690d96ec86a2a810ef115
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-10 10:07:42 +0100

    Add files via upload

---
+++ OBJECTS_ARRAYS.js
@@ -0,0 +1,21 @@
+let contacts = [{
+    name: "Maxwell Wright",
+    phone: "(0191) 719 6495",
+    email: "Curabitur.egestas.nunc@nonummyac.co.uk"
+    }, {
+    name: "Raja Villarreal",
+    phone: "0866 398 2895",
+    email: "posuere.vulputate@sed.com"
+    }, {
+    name: "Helen Richards",
+    phone: "0800 1111",
+    email: "libero@convallis.edu"
+    }];
+let new_contact = {
+name: "Maisie Haley",
+phone: "0913 531 3030",
+email: "risus.Quisque@urna.ca"
+}
+contacts.push(new_contact)
+console.log(contacts[0].name,'/', contacts[0].phone,'/',contacts[0].email)
+console.log(contacts[3].name,'/', contacts[3].phone,'/',contacts[3].email)
