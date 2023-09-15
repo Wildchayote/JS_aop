b3728b61786e5e2091e1e37a7489f34c5ad0b996
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-15 10:34:00 +0100

    Update CONDITIONAL_STATEMENTS.js

---
+++ CONDITIONAL_STATEMENTS.js
@@ -0,0 +1,10 @@
+let user = prompt('Enter Name: ');
+let isFemale = true;
+let State = prompt('State: ');
+
+if (user == 'Adeola' && isFemale ==true && State=='Ogun') {
+    alert('Wife material- Marry her :)')
+}
+else{
+    alert('We don\'t know');
+    }
