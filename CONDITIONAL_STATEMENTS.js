6234a8dd3e3e394f33fd7d14354d693ac85a80e5
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-15 12:18:01 +0100

    Update CONDITIONAL_STATEMENTS.js

--- CONDITIONAL_STATEMENTS.js
+++ CONDITIONAL_STATEMENTS.js
@@ -1,10 +1,18 @@
-let user = prompt('Enter Name: ');
+let user = prompt('Enter name: ');
 let isFemale = true;
-let State = prompt('State: ');
+let state =prompt('Enter state: ');
+let res = (user == 'Adeola' && isFemale == true && state == 'Ogun') ? 'Marry her' : 'Don\'t marry her';
+console.log(`${user},${res}`);
 
-if (user == 'Adeola' && isFemale ==true && State=='Ogun') {
-    alert('Wife material- Marry her :)')
-}
-else{
-    alert('We don\'t know');
-    }
+
+
+// let user = prompt('Enter Name: ');
+// let isFemale = true;
+// let State = prompt('State: ');
+
+// if (user == 'Adeola' && isFemale ==true && State=='Ogun') {
+//     alert('Wife material- Marry her :)')
+// }
+// else{
+//     alert('We don\'t know');
+//     }
