1881a75eb192d6886c369eedc4b390bf3b35d531
Bashir Sanni <bashirsanni4@gmail.com>
2023-09-16 18:20:40 +0100

    Update CONDITIONAL_STATEMENTS.js

--- CONDITIONAL_STATEMENTS.js
+++ CONDITIONAL_STATEMENTS.js
@@ -1,3 +1,26 @@
+let cities = [
+    { name: "New York", population: 18.65e6 },
+    { name: "Cairo", population: 18.82e6 },
+    { name: "Mumbai", population: 19.32e6 },
+    { name: "São Paulo", population: 20.88e6 }, 
+    {name: "Lagos", population: 25.53e6}
+];
+for (let city of cities) {
+    if (city.population > 20e6) {
+        console.log(`${city.name} (${city.population})`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 let user = prompt('Enter name: ');
 let isFemale = true;
 let state =prompt('Enter state: ');
